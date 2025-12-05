a22f75c886355b200a17d219257896eae84a289a
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-12-06 02:04:25 +0600

    Added Simple post method

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
+
 const app = express();
 const port = 5000;
 
+//parser
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World of bangladesh!");
+    res.send("Hello World of express with typescript!");
 });
+
+app.post("/", (req: Request, res: Response) => {
+      console.log(req.body)
+
+  res.status(201).json ({
+    success: true,
+    message: "Server is running successfully",
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
