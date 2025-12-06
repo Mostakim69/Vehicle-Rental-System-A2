ca4bd99bd34fffdae4274877237507c06265f0fd
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-12-06 23:02:39 +0600

    Connect with neon db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,39 @@
 import express, { Request, Response } from "express";
+import {Pool} from "pg"
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 5000;
 //parser
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+
+//DB
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) NOT NULL UNIQUE,
+      password VARCHAR(100) NOT NULL,
+      phone VARCHAR(15),
+      role VARCHAR(50) DEFAULT 'user',
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+
+    );
+  `);
+  console.log("Database initialized");
+}
+
+initDB().catch((err) => console.error("Error initializing database:", err));
+
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World of express with typescript!");
