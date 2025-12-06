090f37ed1652802a0a68260217199e7d366ea657
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-12-07 00:47:28 +0600

    Create DB connect and vehicles information

--- src/server.ts
+++ src/server.ts
@@ -19,16 +19,27 @@ const initDB = async () => {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(150) NOT NULL UNIQUE,
-      password VARCHAR(100) NOT NULL,
-      phone VARCHAR(15),
-      role VARCHAR(50) DEFAULT 'user',
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-
-    );
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL CHECK (LENGTH(password) >= 6),
+      phone VARCHAR(15) NOT NULL,
+      role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer')),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
   `);
-  console.log("Database initialized");
+
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS Vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(150) NOT NULL,
+        type VARCHAR(50) NOT NULL CHECK(type IN ('car', 'bike', 'van','SUV')),
+        registration_number VARCHAR(200) UNIQUE NOT NULL,
+        daily_rent_price INT NOT NULL CHECK(daily_rent_price>0),
+        availability_status VARCHAR(100) NOT NULL CHECK (availability_status IN ('available','booked'))
+        )`);
+
+  // console.log("Database initialized");
 }
 
 initDB().catch((err) => console.error("Error initializing database:", err));
